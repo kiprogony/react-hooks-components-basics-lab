a485c18569f3860837a32398671d540d64f28f08
Daryl Rogony <darylrogony@gmail.com>
2022-05-04 10:45:01 +0300

    create components

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function NavBar() {
+  return (
+  <nav>
+    <a href="#home">I'm a link!</a>
+  </nav>
+  );
+}
+
+export default NavBar;
